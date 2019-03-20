f657ffa3d476d6b5500bf40f47c03e128d0eda23
yuuma <czamy00@gmail.com>
2019-03-20 17:52:58 +0900

    キャラと背景の移動

--- Assets/Script/MainGame/Player/PlayerMove.cs
+++ Assets/Script/MainGame/Player/PlayerMove.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour {
 
+    [SerializeField]
+    GameObject Player;
+
+    private Vector3 pos = new Vector3(0.1f, 0.0f,0.0f);
 	// Use this for initialization
 	void Start () {
-		
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GetTouchPosition();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void GetTouchPosition()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            if(Screen.height/2<= (int)Input.mousePosition.x)
+            {
+                Player.transform.position += pos;
+            }
+            else
+            {
+                Player.transform.position -= pos;
+            }
+
+        }
+
+    }
 }
