e64f7d368ad6a7a6a22ac9a7713ed03cd16f3901
yuuma <czamy00@gmail.com>
2019-03-20 16:43:32 +0900

    色々追加しました

--- Assets/Script/SceneChangeManager.cs
+++ Assets/Script/SceneChangeManager.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneChangeManager : MonoBehaviour {
+public class SceneChangeManager
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum Scenes
+    {
+        Title = 0,
+        MainGame,
+        End
+    }
+
+    private static string[] SceneName =
+    {
+          "Title",
+          "MainGame"
+     };
+
+    public static void SceneChange(Scenes scene)
+    {
+
+        switch (scene)
+        {
+            case Scenes.Title:
+                SceneManager.LoadScene(SceneName[(int)Scenes.Title]);
+                break;
+            case Scenes.MainGame:
+                SceneManager.LoadScene(SceneName[(int)Scenes.MainGame]);
+                break;
+            case Scenes.End:
+                Application.Quit();
+                break;
+            default:
+                break;
+        }
+    }
 }
---
+++ Assets/Script/Title/TitleButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TitleButton : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneChangeManager.SceneChange(SceneChangeManager.Scenes.MainGame);
+    }
+
+    public void EndButton()
+    {
+        SceneChangeManager.SceneChange(SceneChangeManager.Scenes.End);
+    }
+}
