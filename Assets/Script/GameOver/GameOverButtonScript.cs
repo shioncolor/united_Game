b877cfaca96ad2e7a4b19f021b7a68820274a81a
mel225 <mini20cat_drumer@outlook.jp>
2019-03-21 18:55:59 +0900

    [add] フェードアウトの実装

---
+++ Assets/Script/GameOver/GameOverButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverButtonScript : MonoBehaviour {
+    
+	public void BeginButton ()
+    {
+        GameObject panel = GameObject.Find("FadeOutPanel");
+        FadeOut fo = panel.GetComponent<FadeOut>();
+        fo.begin(SceneChangeManager.SceneChange, SceneChangeManager.Scenes.MainGame);
+    }
+	
+	public void HomeButton ()
+    {
+        GameObject panel = GameObject.Find("FadeOutPanel");
+        FadeOut fo = panel.GetComponent<FadeOut>();
+        fo.begin(SceneChangeManager.SceneChange, SceneChangeManager.Scenes.Title);
+    }
+}
---
+++ Assets/Script/Title/TitleButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleButtonScript : MonoBehaviour
+{
+
+    public void BeginButton()
+    {
+        GameObject panel = GameObject.Find("FadeOutPanel");
+        FadeOut fo = panel.GetComponent<FadeOut>();
+        fo.begin(SceneChangeManager.SceneChange, SceneChangeManager.Scenes.MainGame);
+    }
+
+    public void EndButton()
+    {
+        SceneChangeManager.SceneChange(SceneChangeManager.Scenes.End);
+    }
+}
